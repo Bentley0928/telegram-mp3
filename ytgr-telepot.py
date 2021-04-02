5ab85470328c672cdbef66999a3c314561526d1b
Bentley0928 <bentley881@gmail.com>
2021-04-03 01:57:47 +0800

    replace unsupporttede telepot to python-telegram-bot

---
+++ ytgr-telepot.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+import time
+import subprocess
+import telepot
+import os
+import urllib.request
+import re
+import json
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from pathlib import Path
+import youtube_dl
+
+def handle(msg):
+        chat_id = msg['chat']['id']
+        command = msg['text']
+
+        print ("Command from client : %s  " %command)
+
+    #youtube search
+        if command == '/start':
+            bot.sendMessage(chat_id,'輸入yt videolink來下載歌曲(ex: yt your.video.link)')
+        if command.startswith('yt'):
+            param = command[3:]
+            response = urlopen("https://www.youtube.com/results?search_query="+param)
+            data = response.read()
+            response.close()
+            soup = BeautifulSoup(data,"html.parser")
+            vid = soup.find(attrs={'class':'yt-uix-tile-link'})
+            #link = "https://www.youtube.com"+vid['href']
+            global link
+            link = param
+            bot.sendMessage(chat_id,'輸入你想要的檔案名稱(輸入exit來取消)')
+            #watchid = vid['href']
+            #watchid = watchid.replace('/watch?v=','')
+            #title = vid['title']
+            #print (title)
+            print (link)
+            #bot.sendMessage(chat_id,title+"\n"+link)
+        elif command == 'exit':
+            link = ''
+            bot.sendMessage(chat_id, "Bye!")
+        else:
+            options = {
+                'format': 'bestaudio/best',
+                'outtmpl': command +'.mp3',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '320'
+                }]
+            }
+            with youtube_dl.YoutubeDL(options) as ydl:
+                ydl.download([link])
+                print (command)
+                size = Path(command+'.mp3').stat().st_size
+                if size<50000000:
+                    bot.sendAudio(chat_id,audio=open(command + ".mp3",'rb'))
+                    print ("Sent!")
+                    os.remove(command +'.mp3')
+                    link = ""
+                else:
+                    bot.sendMessage(chat_id,'檔案大小超過50MB 請用瀏覽器來下載檔案(Chrome請點選播放器右邊的三個點，按下下載按鈕，謝謝')
+                    os.system("mv "+command+".mp3 ./mp3/")
+
+                    bot.sendMessage(chat_id, "http://mp3.bentley.taipei/"+command+".mp3")
+
+                    bot.sendMessage(chat_id, "success")
+                    print ("Sent!")
+
+    #end youtube search
+
+
+
+#api credentials
+api = open('api.txt','r')
+api_cont = api.read().strip()
+bot = telepot.Bot(api_cont)
+bot.message_loop(handle)
+print ('[+] Server is Listenining [+]')
+print ('[=] Type Command from Telegram [=]')
+
+while 1:
+        time.sleep(10)
--- ytgr.py
+++ ytgr.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import time
 import subprocess
-import telepot
 import os
+import telegram
 import urllib.request
 import re
 import json
@@ -10,17 +10,39 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from pathlib import Path
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import youtube_dl
+flag11 = 0
+api = open('api.txt','r')
+api_cont = api.read().strip()
+bot = telegram.Bot(token=api_cont)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    chat_id = update.message.chat.id
+    print(chat_id)
+    """Send a message when the command /start is issued."""
+    update.message.reply_text("輸入yt videolink來下載歌曲(ex: yt your.video.link)")
+"""
 def handle(msg):
         chat_id = msg['chat']['id']
         command = msg['text']
 
         print ("Command from client : %s  " %command)
-
+"""
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def sendAudio(update, context):
     #youtube search
-        if command == '/start':
-            bot.sendMessage(chat_id,'輸入yt videolink來下載歌曲(ex: yt your.video.link)')
+        global flag11
+        chat_id = update.message.chat.id
+        command = update.message.text
         if command.startswith('yt'):
             param = command[3:]
             response = urlopen("https://www.youtube.com/results?search_query="+param)
@@ -31,17 +53,17 @@ def handle(msg):
             #link = "https://www.youtube.com"+vid['href']
             global link
             link = param
-            bot.sendMessage(chat_id,'輸入你想要的檔案名稱(輸入exit來取消)')
+            bot.sendMessage(chat_id=chat_id,text='輸入你想要的檔案名稱(輸入exit來取消)')
             #watchid = vid['href']
             #watchid = watchid.replace('/watch?v=','')
             #title = vid['title']
             #print (title)
             print (link)
-            #bot.sendMessage(chat_id,title+"\n"+link)
+            flag11 = 1
         elif command == 'exit':
             link = ''
-            bot.sendMessage(chat_id, "Bye!")
-        else:
+            bot.sendMessage(chat_id=chat_id, text="Bye!")
+        elif flag11 == 1:
             options = {
                 'format': 'bestaudio/best',
                 'outtmpl': command +'.mp3',
@@ -56,30 +78,48 @@ def handle(msg):
                 print (command)
                 size = Path(command+'.mp3').stat().st_size
                 if size<50000000:
-                    bot.sendAudio(chat_id,audio=open(command + ".mp3",'rb'))
+                    bot.send_audio(chat_id=chat_id, audio=open(command + ".mp3",'rb'))
                     print ("Sent!")
                     os.remove(command +'.mp3')
                     link = ""
                 else:
-                    bot.sendMessage(chat_id,'檔案大小超過50MB 請用瀏覽器來下載檔案(Chrome請點選播放器右邊的三個點，按下下載按鈕，謝謝')
+                    bot.sendMessage(chat_id,text='檔案大小超過50MB 請用瀏覽器來下載檔案(Chrome請點選播放器右邊的三個點，按下下載按鈕，謝謝')
                     os.system("mv "+command+".mp3 ./mp3/")
 
-                    bot.sendMessage(chat_id, "http://mp3.bentley.taipei/"+command+".mp3")
+                    bot.sendMessage(chat_id=chat_id, text="http://mp3.bentley.taipei/"+command+".mp3")
 
-                    bot.sendMessage(chat_id, "success")
+                    #bot.sendMessage(chat_id=chat_id, "success")
                     print ("Sent!")
-
+                flag11=0
+        elif flag11==0:
+            bot.sendMessage(chat_id, text="輸入格式錯誤ㄛ")
     #end youtube search
 
 
 
-#api credentials
-api = open('api.txt','r')
-api_cont = api.read().strip()
-bot = telepot.Bot(api_cont)
-bot.message_loop(handle)
-print ('[+] Server is Listenining [+]')
-print ('[=] Type Command from Telegram [=]')
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(api_cont, use_context=True)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-while 1:
-        time.sleep(10)
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, sendAudio))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+if __name__ == "__main__":
+    main()
